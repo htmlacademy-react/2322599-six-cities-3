3839ffbfabbfb36a0bcfb27777a0ef8293ba1014
Ekaterina Nesterenko <89526050507@mail.ru>
2025-05-29 20:55:36 +0300

    Добавит маршрутизацию

--- src/const.ts
+++ src/const.ts
@@ -2,3 +2,10 @@ export const Settings = {
   OffersCount: 312,
   City: 'Amsterdam'
 } as const;
+
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id'
+}
