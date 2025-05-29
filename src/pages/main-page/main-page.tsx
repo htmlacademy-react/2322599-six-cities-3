d95338c5959977c48f6ea9cbd2ed3cea25d6f33c
Ekaterina Nesterenko <89526050507@mail.ru>
2025-05-29 21:00:42 +0300

    Обновит ссылку для логотипа

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,5 +1,6 @@
 import OfferCard from '../../components/offer-card/offer-card';
 import { mockOffers } from '../../mocks/offers';
+import Logo from '../../components/logo/logo';
 
 type MainPageProps = {
   offersCount: number;
@@ -13,9 +14,7 @@ function MainPage({ offersCount, city }: MainPageProps): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
