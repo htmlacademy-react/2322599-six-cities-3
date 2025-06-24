74f9d5dc2151f40f99c0eb21c9aef37e5e53c63c
Ekaterina Nesterenko <89526050507@mail.ru>
2025-06-24 22:02:55 +0300

    Создаст компонент SortingOptions и подключит его к MainPage

---
+++ src/components/sorting-options/sorting-options.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+export type SortOption =
+  | 'Popular'
+  | 'Price: low to high'
+  | 'Price: high to low'
+  | 'Top rated first';
+
+type SortingOptionsProps = {
+  currentOption: SortOption;
+  onOptionChange: (option: SortOption) => void;
+};
+
+function SortingOptions({ currentOption, onOptionChange }: SortingOptionsProps): JSX.Element {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const sortOptions: SortOption[] = [
+    'Popular',
+    'Price: low to high',
+    'Price: high to low',
+    'Top rated first'
+  ];
+
+  const toggleOptions = () => setIsOpen(!isOpen);
+  const handleOptionClick = (option: SortOption) => {
+    onOptionChange(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by</span>
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={toggleOptions}
+      >
+        {currentOption}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul className={`places__options places__options--custom ${isOpen ? 'places__options--opened' : ''}`}>
+        {sortOptions.map((option) => (
+          <li
+            key={option}
+            className={`places__option ${option === currentOption ? 'places__option--active' : ''}`}
+            tabIndex={0}
+            onClick={() => handleOptionClick(option)}
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+}
+
+export default SortingOptions;
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -4,12 +4,15 @@ import Map from '../../components/map/map';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import OfferList from '../../components/offer-list/offer-list';
 import CitiesList from '../../components/cities-list/cities-list';
+import SortingOptions from '../../components/sorting-options/sorting-options';
 import { getCurrentCityName, getCurrentCityOffers } from '../../store/selectors';
 import { changeCity } from '../../store/action';
 import type { CityName } from '../../components/cities-list/cities-list';
+import type { SortOption } from '../../components/sorting-options/sorting-options';
 
 function MainPage(): JSX.Element {
   const [activeOfferId, setActiveOfferId] = useState<string | null>(null);
+  const [currentSort, setCurrentSort] = useState<SortOption>('Popular');
   const dispatch = useAppDispatch();
 
   const currentCityName = useAppSelector(getCurrentCityName) as CityName;
@@ -29,6 +32,10 @@ function MainPage(): JSX.Element {
 
   const handleCardMouseLeave = () => {
     setActiveOfferId(null);
+  };
+
+  const handleSortChange = (option: SortOption) => {
+    setCurrentSort(option);
   };
 
   const currentCity = currentCityOffers.length > 0
@@ -63,21 +70,12 @@ function MainPage(): JSX.Element {
               <b className="places__found">
                 {currentCityOffers.length} {currentCityOffers.length === 1 ? 'place' : 'places'} to stay in {currentCityName}
               </b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+
+              <SortingOptions
+                currentOption={currentSort}
+                onOptionChange={handleSortChange}
+              />
+
               <OfferList
                 offers={currentCityOffers}
                 onCardMouseEnter={handleCardMouseEnter}
