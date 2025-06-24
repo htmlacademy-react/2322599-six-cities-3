e1b542dd253b8646af2486baad7fa76951cd4432
Ekaterina Nesterenko <89526050507@mail.ru>
2025-06-24 20:10:45 +0300

    подключит компонент CitiesList к MainPage, изменит город по умолчанию, добавит обновление заголовка с количеством предложений

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { changeCity, fillOffers, updateOfferFavoriteStatus } from './action';
-import { Settings } from '../const';
+import { DEFAULT_CITY } from '../const';
 import type { Offer } from '../types/offers';
 
 export type State = {
@@ -9,7 +9,7 @@ export type State = {
 };
 
 const initialState: State = {
-  currentCityName: Settings.City,
+  currentCityName: DEFAULT_CITY,
   offers: []
 };
 
