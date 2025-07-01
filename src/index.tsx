90bc34592d3238655a66635fd9e141ec5d417268
Ekaterina Nesterenko <89526050507@mail.ru>
2025-07-01 18:05:14 +0300

    Удалит тестовые данные

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
-import { mockOffers } from './mocks/offers';
-import { mockReviews } from './mocks/reviews';
 import { store } from './store';
 
 const root = ReactDOM.createRoot(
@@ -13,10 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App
-        offers={mockOffers}
-        reviews={mockReviews}
-      />
+      <App />
     </Provider>
   </React.StrictMode>
 );
