af2b47e0845b40d3d21c9b754b7b0fde4be92826
Ekaterina Nesterenko <89526050507@mail.ru>
2025-07-03 00:52:27 +0300

    Создаст компонент для отображения ошибки и подключит его

---
+++ src/components/error-message/error-message.tsx
@@ -0,0 +1,13 @@
+import { useAppSelector } from '../../hooks';
+import { getError } from '../../store/selectors';
+import './error-message.css';
+
+function ErrorMessage(): JSX.Element | null {
+  const error = useAppSelector(getError);
+
+  return error
+    ? <div className="error-message">{error}</div>
+    : null;
+}
+
+export default ErrorMessage;
